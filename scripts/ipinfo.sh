93e7520deab69779702210e0dbc8b9c135b12359
Ben-Moriarty <benmoriarty2004@gmail.com>
2026-03-19 09:00:19 -0700

    started creating getmask() for ipinfo.sh to get the subnet mask

--- scripts/ipinfo.sh
+++ scripts/ipinfo.sh
@@ -5,5 +5,29 @@ INFO=$(ip addr show $(ip route show default | awk '{ for(i = 1; i <= NF; i++) { 
 IP=$(cut -d'/' -f1 <<< "$INFO")
 echo $IP
 
-SUBNET=$(cut -d'/' -f2 <<< "$INFO")
-echo $SUBNET
+MASK_LENGTH=$(cut -d'/' -f2 <<< "$INFO")
+echo $MASK_LENGTH
+
+#this is terrible, but it's mine :)))
+function getmask() {
+    CLEARED_BLOCKS=$(($MASK_LENGTH/8))
+    REMAINDER=$(($MASK_LENGTH - 8 * $CLEARED_BLOCKS))
+
+    declare DECIMAL
+    for ((i = 0; i <= $CLEARED_BLOCKS; i++)); do
+        BLOCK=$(cut -d'.' -f$(($i+1)) <<< "$IP")
+        
+        DECIMAL+=$BLOCK
+        DECIMAL+="."
+    done
+
+    if (( $REMAINDER > 0 )); then
+        echo great
+    else
+        echo "add zero"
+    fi
+
+    echo $DECIMAL
+}
+
+getmask

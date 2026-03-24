70b32dcd6c7435a4d015f4d0f4f0614c807af457
Ben Moriarty <benmoriarty2004@gmail.com>
2026-03-24 13:33:04 -0700

    added CPU stress test to check for throttling

--- scripts/hw_health.ps1
+++ scripts/hw_health.ps1
@@ -1,3 +1,14 @@
-$Health = (Get-PhysicalDisk).HealthStatus
+$DriveHealth = (Get-PhysicalDisk).HealthStatus
 
-Write-Host $Health
+$MaxCPU = (Get-CimInstance -ClassName Win32_Processor).MaxClockSpeed
+Write-Host $MaxCPU
+
+$EndTime = (Get-Date).AddMinutes(20)
+$x = 1
+While ((Get-Date) -lt $EndTime) {
+    $x * $x
+    $x++
+}
+
+$CurCPU = (Get-CimInstance -ClassName Win32_Processor).CurrentClockSpeed
+Write-Host $CurCPU

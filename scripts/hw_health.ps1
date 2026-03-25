cc9b8d02f57147f7f688ec62dbe375a2273f86d6
Ben Moriarty <benmoriarty2004@gmail.com>
2026-03-24 19:42:42 -0700

    added memory capacity check and created hash table for results

--- scripts/hw_health.ps1
+++ scripts/hw_health.ps1
@@ -1,14 +1,25 @@
+$Results = @{}
+
 $DriveHealth = (Get-PhysicalDisk).HealthStatus
+$Results['DriveHealth'] = $DriveHealth
+
 
 $MaxCPU = (Get-CimInstance -ClassName Win32_Processor).MaxClockSpeed
-Write-Host $MaxCPU
-
 $EndTime = (Get-Date).AddMinutes(20)
+
 $x = 1
 While ((Get-Date) -lt $EndTime) {
     $x * $x
     $x++
 }
-
 $CurCPU = (Get-CimInstance -ClassName Win32_Processor).CurrentClockSpeed
-Write-Host $CurCPU
+
+if ($CurCPU -lt 0.5 * ($MaxCPU)) {
+    $Results['CPU'] = 'Throttling'
+}
+else {
+    $Results['CPU'] = 'NoThrottling'
+}
+
+$MemCapGB = (Get-CimInstance Win32_PhysicalMemory).Capacity / 1GB
+$Results['Memory'] = $MemCapGB

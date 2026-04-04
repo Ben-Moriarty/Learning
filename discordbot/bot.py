2a202c5c2f6fa3e0b937167630d6177f64691c6e
Ben Moriarty <benmoriarty2004@gmail.com>
2026-04-04 00:19:41 -0700

    env

--- discordbot/bot.py
+++ discordbot/bot.py
@@ -2,6 +2,10 @@ import aiohttp
 import discord
 from discord.ext import commands
 
+import os
+
+discord_key = os.getenv('DISCORD_KEY')
+
 intents = discord.Intents.default()
 intents.message_content = True
 
@@ -23,6 +27,9 @@ async def request(ctx, *, message):
         async with session.post(webhook_url, json=data) as response:
             if response.status == 200:
                 result = await response.text()
-                await ctx.send(f"message sent success: {result}")
+                shortened_message = result[:1900]
+                await ctx.send(f'{shortened_message}')
             else:
                 await ctx.send(f'Error {response.status}')
+
+bot.run(discord_key)
